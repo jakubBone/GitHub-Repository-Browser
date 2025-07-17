25af6aa532c2c02248020d3ea7fa0ee575dc642e
jakubBone <jakub.bone1990@gmail.com>
2025-07-17 10:01:00 +0200

    initial commit

---
+++ src/main/java/com/jakubbone/github_repository_browser/GithubRepositoryBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.jakubbone.github_repository_browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoryBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubbone/github_repository_browser/GithubRepositoryBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubbone.github_repository_browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

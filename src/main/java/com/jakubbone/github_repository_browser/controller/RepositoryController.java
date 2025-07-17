98a9af555747bd2035b0489aa867e46e51bb996e
jakubBone <jakub.bone1990@gmail.com>
2025-07-17 11:40:17 +0200

    feat: implemented initial logic for conteroller and service

--- src/main/java/com/jakubbone/github_repository_browser/controller/RepositoryController.java
+++ src/main/java/com/jakubbone/github_repository_browser/controller/RepositoryController.java
@@ -1,11 +1,23 @@
 package com.jakubbone.github_repository_browser.controller;
 
+import com.jakubbone.github_repository_browser.dto.RepositoryResponse;
+import com.jakubbone.github_repository_browser.service.RepositoryService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("github/")
 public class RepositoryController {
+    private final RepositoryService repositoryService;
 
+    public RepositoryController(RepositoryService repositoryService) {
+        this.repositoryService = repositoryService;
+    }
 
+    public List<RepositoryResponse> getRepos(@PathVariable String ownerLogin){
+        repositoryService.findAndReturnRepos(ownerLogin);
+    }
 }
--- src/main/java/com/jakubbone/github_repository_browser/service/RepositoryService.java
+++ src/main/java/com/jakubbone/github_repository_browser/service/RepositoryService.java
@@ -1,4 +1,14 @@
 package com.jakubbone.github_repository_browser.service;
 
+import com.jakubbone.github_repository_browser.dto.RepositoryResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RepositoryService {
+
+    public List<RepositoryResponse> findAndReturnRepos(String ownerLogin){
+
+    }
 }

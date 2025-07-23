661d70bc86f82a313759766898920f8dd5ecdbde
jakubBone <jakub.bone1990@gmail.com>
2025-07-23 15:53:02 +0200

    feat: implemented authorization header to ApiClient

--- src/main/java/com/jakubbone/repository_browser/config/RestClientConfig.java
+++ src/main/java/com/jakubbone/repository_browser/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.jakubbone.repository_browser.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,10 +8,14 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+    @Value("${API_TOKEN}")
+    private String apiToken;
+
     @Bean
     public RestClient restClient(){
         return RestClient.builder()
                 .baseUrl("https://api.github.com")
+                .defaultHeader("Autorization","Bearer " + apiToken)
                 .build();
     }
 }

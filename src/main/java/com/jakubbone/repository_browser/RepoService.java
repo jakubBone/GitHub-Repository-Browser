ce9ed739dbd13c75fa6ddc698a5076635e6ca883
jakubBone <jakub.bone1990@gmail.com>
2025-07-28 09:43:45 +0200

    fix: added logging to service

--- src/main/java/com/jakubbone/repository_browser/RepoService.java
+++ src/main/java/com/jakubbone/repository_browser/RepoService.java
@@ -5,6 +5,7 @@ import com.jakubbone.repository_browser.api.ApiClient.Repo;
 import com.jakubbone.repository_browser.api.ApiClient.Branch;
 import com.jakubbone.repository_browser.dto.RepoResponse;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @Service
+@Log4j2
 public class RepoService {
     private final ApiClient client;
 
@@ -19,8 +21,9 @@ public class RepoService {
         this.client = client;
     }
     public List<RepoResponse> getRepos(String owner) {
-
+        log.debug("Getting repositories for owner: {}", owner);
         List<Repo> repos = client.getRepoForOwner(owner);
+        log.debug("Found {} repositories for owner: {}", repos.size(), owner);
 
         return repos.stream()
                 .filter(repo -> !repo.fork())
